33cc8e0b8289ef0a87402b4e80b88bb7fecd53e5
Razielkek <zzzm0naxzzz@gmail.com>
2018-12-25 18:52:42 +0300

    Update ConstValue.java

--- TestGMailByAppium-master/src/test/java/util/ConstValue.java
+++ TestGMailByAppium-master/src/test/java/util/ConstValue.java
@@ -2,7 +2,7 @@ package util;
 
 public final class ConstValue {
     public static final String user = "zzzm0naxzzz@gmail.com";
-    public static final String password = "ndodontiasab1";
+    public static final String password = "sab1";
     public static final String email = "zzzm0naxzzz@gmail.com";
     public static final String appPackage = "com.google.android.gm";
     public static final String appActivity = "ConversationListActivityGmail";
